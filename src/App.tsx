d6e09241d15e01b43d679fb7d2d6335c8fed7355
jia40 <lsc7439@g.eulji.ac.kr>
2026-08-31 00:58:19 +0900

    [FEATURE] 회원가입 페이지 UI 구현#6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
-  return <LoginPage />
+  return window.location.pathname === '/signup' ? <SignupPage /> : <LoginPage />
 }
 
 export default App
